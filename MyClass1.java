a79782fe144fbfd1fc3772e7b29f96f326bb1c26
aryansaraswat <46528597+aryansaraswat@users.noreply.github.com>
2019-01-09 22:10:15 +0530

    Add files via upload

---
+++ MyClass1.java
@@ -0,0 +1,12 @@
+package lesson1;
+ ;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+char d ='d';
+int i ='d';
+System.out.println("Integer representation of entered charecter is");
+System.out.println(i);
+
+	}}
---
+++ Third.java
@@ -0,0 +1,14 @@
+
+public class Third {
+	public static void main(String[] args) {
+long minutes= 4555222;
+float years= minutes/525600;
+float months= minutes/43800;
+float days=minutes/1440;
+System.out.println("Total mo. of years are" + years);
+System.out.println("Total mo. of months are" + months);
+System.out.println("Total mo. of days are" + days);
+
+
+	}
+}
