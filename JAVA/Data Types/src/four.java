6833dcd26b4d4660d09c2b6929e0bd00ce28770b
aryansaraswat <46528597+aryansaraswat@users.noreply.github.com>
2019-01-10 20:46:28 +0530

    Add files via upload

---
+++ JAVA/Data Types/src/four.java
@@ -0,0 +1,12 @@
+class four{
+	public static void main(String agrs[]){
+		long minutes = 4555222;
+		float years = minutes/525600f;
+		float months = minutes/43800f;
+		float days = minutes/1440f;
+		System.out.println("Total no. of years are:" + years);
+		System.out.println("Total no. of months are:" + months);
+		System.out.println("Total no. of days are:" + days);
+
+	}
+}
---
+++ JAVA/Data Types/src/one.java
@@ -0,0 +1,7 @@
+class one{
+	public static void main(String args[]){
+		int num1 = 10, num2 = 20;
+		System.out.println("The sum is");
+		System.out.println(num1+num2);
+	}
+}
